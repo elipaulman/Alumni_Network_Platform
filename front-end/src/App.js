1d812e92ae449929fa2bc8cefdf2c16eccd03fa7
Elijah Mendez <elijahmendez2022@gmail.com>
2024-11-01 17:57:45 -0400

    Added CenteredTitle comp for main page.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import CenteredTitle from './components/CenteredTitle';
 
 function App() {
   return (
-
     <div>
       <Navbar />
+      <CenteredTitle text="LMCC Alumni Network" />
     </div>
   );
 }
