a4b84a63a6c3cce62a7b0d1aad44efd9a6f35b55
benjamindrussell <ben@benjamindrussell.com>
2024-11-01 19:03:15 -0400

    fully separated main db route and tsting route

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import indexRouter from './routes/index.js';
-import dbRouter from './routes/dbTest.js'
+import dbTestRouter from './routes/dbTest.js'
 import * as dotenv from 'dotenv';
 import mongoose, { connect, Model, Schema } from 'mongoose';
 dotenv.config()
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(morgan());
 app.use(express.json());
 app.use('/', indexRouter);
-app.use('/db', dbRouter);
+app.use('/test', dbTestRouter);
 
 const PORT = process.env.PORT || 5050;
 const URI = process.env.ATLAS_URI || '';
