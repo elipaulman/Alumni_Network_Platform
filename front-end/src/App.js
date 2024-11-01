dd0176bfb7f8c16ec850d9c97afea13d289a6722
Elijah Mendez <elijahmendez2022@gmail.com>
2024-11-01 17:38:58 -0400

    First navbar implementation.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-4xl">Hello</h1>
+
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
