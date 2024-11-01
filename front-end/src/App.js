abe043f85a57e974e6e4fbe54ff967cc473b42e7
Elijah Mendez <elijahmendez2022@gmail.com>
2024-11-01 18:22:55 -0400

    Adding routing so everyone can see their work now!

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import CenteredTitle from './components/CenteredTitle';
+import About from './pages/About';
+import Directory from './pages/Directory';
+import Events from './pages/Events';
+import Feed from './pages/Feed';
+import Listings from './pages/Listings';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <Navbar />
-      <div className="flex-1 flex items-center justify-center">
-        <CenteredTitle text="LMCC Alumni Network" />
+    <Router>
+      <div className="min-h-screen flex flex-col bg-gray-100">
+        <Navbar />
+        <div className="flex-1 flex items-center justify-center p-6">
+          <Routes>
+            <Route path="/" element={<CenteredTitle text="LMCC Alumni Network" />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/directory" element={<Directory />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/listings" element={<Listings />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
