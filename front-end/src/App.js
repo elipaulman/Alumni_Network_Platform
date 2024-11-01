95a6f1dc6f51580dd3f9ec59b4dfe0449c51e5a8
Elijah Mendez <elijahmendez2022@gmail.com>
2024-11-01 18:06:22 -0400

    Commit so the team can merge so we are all on the same page.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,9 +4,11 @@ import CenteredTitle from './components/CenteredTitle';
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <Navbar />
-      <CenteredTitle text="LMCC Alumni Network" />
+      <div className="flex-1 flex items-center justify-center">
+        <CenteredTitle text="LMCC Alumni Network" />
+      </div>
     </div>
   );
 }
