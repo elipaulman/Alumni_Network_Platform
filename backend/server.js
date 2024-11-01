0b1f91817ad5d90566d0fb4f3904f6fb373b1d25
benjamindrussell <ben@benjamindrussell.com>
2024-11-01 16:24:48 -0400

    initial scaffolding of express backend with routes and dotenv

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import indexRouter from './routes/index.js';
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+const app = express();
+app.use(cors());
+app.use(morgan());
+app.use(express.json());
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 5050;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
