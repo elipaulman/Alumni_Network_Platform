5e2714faee98a1862a99a2085fc8d5d32fa02e49
AHMED_HUSSEIN <ahmedua2003@gmail.com>
2024-11-01 20:16:36 -0400

    Base Start of the Calendar cards

--- front-end/src/pages/Events.jsx
+++ front-end/src/pages/Events.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React from "react";
+import "./Events.css";
 
 const Events = () => {
   return (
-    <div>Events</div>
-  )
-}
+    <>
+      <h1 class="text-4x1 font-bold mb-4 text-gray-800">This month</h1>
 
-export default Events
+<div className="row flex flex-direction-row">
+<div className="firstRow">
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Drawing Seminar By Hamed</p>
+        </div>
+        
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Water Painting By Ben</p>
+        </div>
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Dancing workshop By Akib</p>
+        </div>
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Describe the event for this day</p>
+        </div>
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Describe the event for this day</p>
+        </div>
+        </div>
+        <div className="secondRow col">
+          <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+            <h2 className="text-2xl font-bold mb-4 text-gray-800">
+              Today's Event
+            </h2>
+            <p>Describe the event for this day</p>
+        </div>
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Describe the event for this day</p>
+        </div>
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Describe the event for this day</p>
+        </div>
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Describe the event for this day</p>
+        </div>
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Describe the event for this day</p>
+        </div>
+        <div class="flex flex-wrap justify-center border border-gray-300 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block bg-white">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            Today's Event
+          </h2>
+          <p>Describe the event for this day</p>
+        </div>
+        </div>
+</div>
+      
+    </>
+  );
+};
+
+export default Events;
