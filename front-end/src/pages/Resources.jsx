05f1f4aed4e4d6f6c491cb31f6b20c99a5edc3e1
Elijah Mendez <elijahmendez2022@gmail.com>
2024-11-02 03:18:48 -0400

    UI Button color fix on Resources page.

--- front-end/src/pages/Resources.jsx
+++ front-end/src/pages/Resources.jsx
@@ -85,10 +85,10 @@ const Resources = () => {
         {categories.map((category, index) => (
           <button
             key={index}
-            className={`mx-2 px-4 py-2 rounded-full shadow-lg transition duration-200 ease-in-out ${
+            className={`mx-2 px-6 py-3 rounded-full shadow-lg transform transition-transform duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-300 hover:scale-105 hover:shadow-xl ${
               selectedCategory === category
-                ? 'bg-blue-500 text-white'
-                : 'bg-white text-blue-500 border border-blue-500'
+                ? 'bg-[#00BDF2] text-white'
+                : 'bg-white text-[#00BDF2] border border-[#00BDF2]'
             }`}
             onClick={() => handleCategoryClick(category)}
           >
