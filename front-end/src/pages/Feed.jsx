8a87117b64467eca516ee0d363563007f57df5a8
Elijah Paulman <eli.paulman@gmail.com>
2024-11-02 08:42:32 -0400

    Changed feed profile pics

--- front-end/src/pages/Feed.jsx
+++ front-end/src/pages/Feed.jsx
@@ -282,7 +282,7 @@ const Feed = () => {
           >
             <div className="p-4 flex items-center">
               <img
-                src={`https://i.pravatar.cc/150?img=${post._id}`}
+                src={`https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg`}
                 alt="User avatar"
                 className="w-10 h-10 rounded-full mr-4 border-2 border-[#00BDF2]"
               />
