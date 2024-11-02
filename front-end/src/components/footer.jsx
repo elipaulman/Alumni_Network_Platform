44c0f5341bdee9dc74dcf314851b7905ef1f53c4
AHMED_HUSSEIN <ahmedua2003@gmail.com>
2024-11-02 06:59:44 -0400

    Completed product for Footer

--- front-end/src/components/footer.jsx
+++ front-end/src/components/footer.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import { TiSocialInstagram } from "react-icons/ti";
+import { TiSocialTwitter } from "react-icons/ti";
+import { SlSocialFacebook } from "react-icons/sl";
+import { SlSocialLinkedin } from "react-icons/sl";
 
 const Footer = () => {
   return (
     <footer className="bg-[#00BDF2] text-white p-5 text-center">
       <div className="social-icons mb-3 flex justify-center">
-        <a href="" target="_blank" aria-label="Instagram" className="mx-2">
-          <img src="" alt="Instagram Icon" className="w-6 h-6" />
+        <a href="https://www.instagram.com/lmcc_nyc/" target="https://www.instagram.com/lmcc_nyc/" aria-label="Instagram" className="mx-2">
+            <TiSocialInstagram className="w-6 h-6" />
         </a>
-        <a href="" target="_blank" aria-label="X (formerly Twitter)" className="mx-2">
-          <img src="" alt="X Icon" className="w-6 h-6" />
+        <a href="https://x.com/LMCC" target="https://x.com/LMCC" aria-label="X (formerly Twitter)" className="mx-2">
+            <TiSocialTwitter className="w-6 h-6" />
         </a>
-        <a href="" target="_blank" aria-label="Facebook" className="mx-2">
-          <img src="" alt="Facebook Icon" className="w-6 h-6" />
+        <a href="https://www.facebook.com/LMCCNYC/" target="https://www.facebook.com/LMCCNYC/" aria-label="Facebook" className="mx-2">
+            <SlSocialFacebook className="w-6 h-6" />
         </a>
-        <a href="" target="_blank" aria-label="LinkedIn" className="mx-2">
-          <img src="" alt="LinkedIn Icon" className="w-6 h-6" />
+        <a href="https://www.linkedin.com/company/lower-manhattan-cultural-council/" target="https://www.linkedin.com/company/lower-manhattan-cultural-council/" aria-label="LinkedIn" className="mx-2">
+            <SlSocialLinkedin className="w-6 h-6" />
         </a>
       </div>
-      <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+      <p>&copy; 2024 Lower Manhattan Cultural Council, LMCC. All rights reserved.</p>
     </footer>
   );
 };
