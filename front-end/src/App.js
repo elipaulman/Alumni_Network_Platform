8a5d98bedb927505ec42e782101dff1ee6c8cfb1
Akib <akibmahdi1@gmail.com>
2024-11-01 20:29:28 -0400

    stashing current changes before pulling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import CenteredTitle from './components/CenteredTitle';
@@ -8,24 +8,26 @@ import Directory from './pages/Directory';
 import Events from './pages/Events';
 import Feed from './pages/Feed';
 import Listings from './pages/Listings';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col bg-gray-100">
-        <Navbar />
-        <div className="flex-1">
-          <Routes>
-            <Route path="/" element={<CenteredTitle text="LMCC Alumni Network" />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/directory" element={<Directory />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/feed" element={<Feed />} />
-            <Route path="/listings" element={<Listings />} />
-          </Routes>
-        </div>
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      <Navbar />
+      <div className="flex-1">
+        <Routes>
+          <Route path="/" element={<CenteredTitle text="LMCC Alumni Network" />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/directory" element={<Directory />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/listings" element={<Listings />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
-    </Router>
+    </div>
   );
 }
 
