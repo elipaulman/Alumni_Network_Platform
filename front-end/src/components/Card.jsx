54c27985266e19fb8d0b94fd552453bffa5e0505
Elijah Mendez <elijahmendez2022@gmail.com>
2024-11-01 22:53:55 -0400

    CONNECTED DB W AXIOS AND DISPLAYED OPPURTUNITIES W/ BEN TEAM COMMIT

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({ data }) => {
   return (
     <li className="bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden transition-transform transform hover:scale-105">
       <div className="p-4">
-        <h2 className="text-2xl font-semibold mb-2 text-gray-800">{data.name}</h2>
+        <h2 className="text-2xl font-semibold mb-2 text-gray-800">{data.opportunityName}</h2>
         {data.artCategory && <p className="text-gray-500 mb-2">{data.artCategory}</p>}
         {data.description && <p className="text-gray-600 mb-2">{data.description}</p>}
         {data.category && <p className="text-gray-500 mb-2">{data.category}</p>}
